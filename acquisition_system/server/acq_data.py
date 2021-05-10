b7d1e5c4d76e722e07693b88ef79f4c55bacf157
rans <ranshuang0621@163.com>
2021-05-10 16:12:41 +0800

    change

--- acquisition_system/server/acq_data.py
+++ acquisition_system/server/acq_data.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__, template_folder='../web/html', static_folder="../web", static_url_path="")
+count = 0
+video_list = ["demo1", "demo2"]
 
 
 @app.route('/')
@@ -25,18 +27,28 @@ def rest_info():
     return render_template('rest_info.html')
 
 
-@app.route('/rest')
+@app.route('/rest', methods=['POST','GET'])
 def rest():
-    return render_template('rest.html')
+    global count
+    video_name = video_list[count]
+    return render_template('rest.html', video_name=video_name)
+
+
+@app.route('/rest/<video_name>')
+def video_name():
+    # return render_template('rest.html')
+    return redirect(url_for('video'))
 
 
 @app.route('/video')
 def video():
+    print(request.args.get("v_name"))
     return render_template('video.html')
 
 
 @app.route('/subjective_evaluation')
 def subjective_evaluation():
+    print("subjective_evaluation")
     return render_template('subjective_evaluation.html')
 
 
@@ -44,11 +56,18 @@ def subjective_evaluation():
 def submit_evaluation():
     user = request.form
     print(user)
-    return redirect(url_for('end'))
+    global count
+    if count == len(video_list) - 1:
+        return redirect(url_for('end'))
+    elif count < len(video_list) - 1:
+        count = count + 1
+        return redirect(url_for('rest'))
 
 
 @app.route('/end')
 def end():
+    global count
+    count = 0
     return render_template('end.html')
 
 
